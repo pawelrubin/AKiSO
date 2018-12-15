2c8129e964cc9d95ff77c45e553f38e2449e082d
pawelrubin <pawelrubin19@gmail.com>
2018-12-16 00:59:21 +0100

    Potężna aktualizacja.
    - mutex,
    - join,
    - struktura z danymi globalna,
    - czyszczenie alokowanej pamięci,
    - potężne komentarze

--- Lista5/zad2.c
+++ Lista5/zad2.c
@@ -3,35 +3,55 @@
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
-#define ONE_OVER_THOUSAND 1E-3 
-#define MILION 1E6
 
-int rows_calculated = 0;
-
+/*
+The following structure contains the necessary information to allow
+the function "squared_boolean_matrix_multithread_multiplication"
+to access its input data and place its output into the structure.
+*/
 typedef struct {
   int **matrix1;
   int **matrix2;
   int **result;
   int size;
-} Zad2Matrices;
+} DATA;
 
+/* Declare globally accessible variables and the mutex. */
+DATA matrix;
+int available_row = 0;
+pthread_mutex_t mutex_counter;
+
+/*
+This function is activated when the thread is created.
+All input is obtained from a structure of type DATA.
+The output - product matrix - is also written into this structure.
+The function calculate every first available row.
+*/
 void *squared_boolean_matrix_multithread_multiplication(void *vargp) {
-  Zad2Matrices *matrix = (Zad2Matrices*)&vargp;
-
-  for (int i = rows_calculated ; i < rows_calculated; rows_calculated++) {
-    for (int j = 0; j < matrix->size; j++) {
+  int i;
+  while(available_row < matrix.size) {
+    pthread_mutex_lock (&mutex_counter);
+      i = available_row;
+      available_row++;
+    pthread_mutex_unlock (&mutex_counter);
+    for (int j = 0; j < matrix.size; j++) {
       int value = 0;
-      for (int k = 0; k < matrix->size; k++) {
-        value |= (matrix->matrix1[i][k] & matrix->matrix2[k][j]);
+      for (int k = 0; k < matrix.size; k++) {
+        value |= (matrix.matrix1[i][k] & matrix.matrix2[k][j]);
         if (value) {
           break;
         }
       }
-      matrix->result[i][j] = value;
+      matrix.result[i][j] = value;    
     }
   }
+  pthread_exit((void*) 0);
 }
 
+/* 
+This function allocate memory for the int matrix 
+for given width and height.
+*/
 int ** malloc_matrix(int width, int height) {
   int **matrix = (int **)malloc(height * sizeof(int *)); 
   for (int i = 0; i < height; i++) {
@@ -40,6 +60,9 @@ int ** malloc_matrix(int width, int height) {
   return matrix;
 }
 
+/*
+This function randomly fill given matrix with 0 or 1.
+*/
 void random_fill_boolean_matrix(int **matrix, int width, int height) {
   time_t t;
   srand(time(&t));
@@ -47,6 +70,12 @@ void random_fill_boolean_matrix(int **matrix, int width, int height) {
     for (int j = 0; j < width; j++) {
       matrix[i][j] = (rand()%2);
     }
+  }
+}
+
+void free_2d_array(int **matrix, int height) {
+  for (int i = 0; i < height; i++) {
+    free(matrix[i]);
   }
 }
 
@@ -58,9 +87,10 @@ int main(int argc, char *argv[]) {
 
   int matrix_size = atoi(argv[1]);
   int num_of_threads = atoi(argv[2]);
+  printf("size: %d, num of threads: %d\n", matrix_size, num_of_threads);
+  printf("calculating...\n");
 
-  Zad2Matrices matrix; 
-
+  // initialising the DATA struct
   matrix.matrix1 = malloc_matrix(matrix_size, matrix_size);
   matrix.matrix2 = malloc_matrix(matrix_size, matrix_size);
   matrix.result = malloc_matrix(matrix_size, matrix_size);
@@ -69,11 +99,26 @@ int main(int argc, char *argv[]) {
   random_fill_boolean_matrix(matrix.matrix1, matrix_size, matrix_size);
   random_fill_boolean_matrix(matrix.matrix2, matrix_size, matrix_size);
 
+  pthread_attr_t attr;
   pthread_t ptIDs[num_of_threads];
+  pthread_mutex_init(&mutex_counter, NULL);        
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+  void *status;
 
   for (int i = 0; i < num_of_threads; i++) {
-    pthread_create(&ptIDs[i], NULL, squared_boolean_matrix_multithread_multiplication, (void *)&matrix);
+    pthread_create(&ptIDs[i], &attr, squared_boolean_matrix_multithread_multiplication, NULL);
   }
 
+  // waiting on the other threads
+  for (int i = 0; i <num_of_threads; i++) {
+    pthread_join(ptIDs[i], &status);
+  }
+
+  // cleanup
+  pthread_mutex_destroy(&mutex_counter);
+  free_2d_array(matrix.matrix1, matrix_size);
+  free_2d_array(matrix.matrix2, matrix_size);
+  free_2d_array(matrix.result, matrix_size);
   return 0;
 }
