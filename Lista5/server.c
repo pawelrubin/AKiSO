7b48178e238e7feedf336e3ea3483dcd2e1781d7
pawelrubin <pawelrubin19@gmail.com>
2018-12-16 13:36:01 +0100

    Powiadomienie o połaczeniu nowego użytkownika.

--- Lista5/server.c
+++ Lista5/server.c
@@ -34,6 +34,9 @@ typedef struct {
   int new_socket;
 } Server;
 
+// Globally accesible structure of data
+Server server;
+
 void initialise_all_to_0(int *array, int size) {
   for (int i = 0; i < size; i++) {
     array[i] = 0;
@@ -93,8 +96,52 @@ char * int_to_string(int number, int base) {
   return result;
 }
 
+void show_users(int socket) {
+  int count = 1;
+  char *message = "\nConnected users:\n";
+  send(socket, message, strlen(message), 0);
+  for (int j = 0; j < MAX_CLIENTS; j++) {
+    if (server.clients.sockets[j] != 0) {
+      if (server.clients.sockets[j] != socket) {
+        char nickname[30];
+        strncpy(nickname, "", sizeof(nickname));
+        strcat(nickname, server.clients.nicknames[j]);
+        message = int_to_string(count, 10);
+        // strncpy(message, int_to_string(count, 10), sizeof(message));
+        strcat(message, ". ");
+        strcat(message, nickname);
+        strcat(message, "\n");
+        send(socket, message, strlen(message), 0);
+        count++;
+      } else {
+        message = int_to_string(count, 10);
+        strcat(message, ". ");
+        strcat(message, "You\n");
+        send(socket, message, strlen(message), 0);
+        count++;
+      }
+    }
+  }
+  send(socket, "\n", strlen("\n"), 0);
+}
+
+void user_connected_notification(int id) {
+  char *new_user_nickname = server.clients.nicknames[id];
+
+  for (int i = 0; i < MAX_CLIENTS; i++) {
+    if (i != id) {
+      int socket = server.clients.sockets[i];
+      char message[128];
+      strncpy(message, "\n\033[1m", sizeof(message));
+      strcat(message, new_user_nickname);
+      strcat(message, " connected\033[0m\n\n");
+      send(socket, message, strlen(message), 0);
+    }
+  }
+}
+
 int main(int argc, char const *argv[]) { 
-  Server server;
+  
   char buffer[BUFFER_SIZE];
   char nick[30];
   int started[MAX_CLIENTS];
@@ -163,25 +210,10 @@ int main(int argc, char const *argv[]) {
           strcat(greet, nick);
           strcat(greet, "\n");
           send(server.clients.sockets[i], greet, strlen(greet), 0);
-           
-          int count = 1;
-          message = "Connected users:\n";
-          send(server.clients.sockets[i], message, strlen(message), 0);
-          for (int j = 0; j < MAX_CLIENTS; j++) {
-            if (server.clients.sockets[j] != 0) {
-              if (j != i) {
-                char nickname[30];
-                strncpy(nickname, int_to_string(i, 10), sizeof(nickname));
-                strcat(nickname, ". ");
-                strcat(nickname, server.clients.nicknames[j]);
-                strcat(nickname, "\n");
-                send(server.clients.sockets[i], nickname, strlen(nickname), 0);
-                count++;
-              }
-            }
-          }
-          
-       
+
+          user_connected_notification(i);
+          show_users(server.clients.sockets[i]); 
+
           break;
         }
       }
@@ -193,10 +225,6 @@ int main(int argc, char const *argv[]) {
       server.sd = server.clients.sockets[i];   
             
       if (FD_ISSET(server.sd, &server.readfds)) {
-        // char response[BUFFER_SIZE];
-        // int len = recv(server.sd, response, sizeof(response), 0);
-        // response[len] = '\0';
-        // printf("%s", response);
         int len;
         if ((len = recv(server.sd, buffer, sizeof(buffer), 0)) == 0) {   
             getpeername(server.sd, (struct sockaddr*)&server.address,(socklen_t*)&server.addr_len);   
@@ -206,28 +234,30 @@ int main(int argc, char const *argv[]) {
             for (int j = 0; j < MAX_CLIENTS; j++) {
             if (j != i) {
               int a_socket = server.clients.sockets[j];
-              char *client = int_to_string(i, 10);
+              char *client = server.clients.nicknames[i];
               char *dis = " disconnected\n";
               send(a_socket, client, strlen(client), 0);
               send(a_socket, dis, strlen(dis), 0);
             }
           }
         } else {
-          buffer[len] = '\0';
           // printf("wiadomosc od klienta nr %d: %s", i, buffer);
-          printf("wiadomosc od <%s>: %s", server.clients.nicknames[i], buffer);
+          buffer[len - 2] = '\0';
+          printf("wiadomosc od <%s>: <%s>\n", server.clients.nicknames[i], buffer);
+          // buffer[len-1] = '\0';
           for (int j = 0; j < MAX_CLIENTS; j++) {
             if (j != i) {
               int a_socket = server.clients.sockets[j];
               if (a_socket != 0) {              
                 send(a_socket, server.clients.nicknames[i], strlen(server.clients.nicknames[i]), 0);
                 send(a_socket, ": ", strlen(": "), 0);
-                send(a_socket, buffer, strlen(buffer), 0); 
+                send(a_socket, buffer, strlen(buffer), 0);
+                send(a_socket, "\n", strlen("\n"), 0);
               }
             }
           }
         }
-      }     
+      } 
     }
   }
 
